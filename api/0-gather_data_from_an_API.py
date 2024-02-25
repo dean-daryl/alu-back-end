7bbf77c6d7df10884c21fee29bdce837fab89018
dean-daryl <deandaryl7@gmail.com>
2024-02-25 15:02:49 +0200

    fix linting error

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,24 +1,38 @@
 #!/usr/bin/python3
-"""get TODO list"""
-
+"""
+    python script that returns TODO list progress for a given employee ID
+"""
 import json
 import requests
-import sys
+from sys import argv
+
+
 if __name__ == "__main__":
-    link = "https://jsonplaceholder.typicode.com/users/{}".format(sys.argv[1])
-    res = requests.get(link, verify=False)
-    user = json.loads(res.text)
-    num = sys.argv[1]
-    link = "https://jsonplaceholder.typicode.com/users/{}/todos".format(num)
-    res = requests.get(link, verify=False)
-    todos = json.loads(res.text)
-    done = []
-    for i in todos:
-        if i['completed']:
-            done.append(i)
+    # Request user info by employee ID
+    employee_id = argv[1]
+    
+    user_info_url = 'https://jsonplaceholder.typicode.com/users/{}/'.format(employee_id)
+    user_todos_url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(employee_id)
+    
+    # Request user info
+    res_user_info = requests.get(user_info_url, verify=False)
+    employee = json.loads(res_user_info.text)
+    employee_name = employee.get("name")
+
+    # Request user's TODO list
+    res_user_todos = requests.get(user_todos_url, verify=False)
+    employee_todos = json.loads(res_user_todos.text)
+
+    # Dictionary to store task status in boolean format
+    tasks = {dictionary.get("title"): dictionary.get("completed") for dictionary in employee_todos}
+
+    # Return name, total number of tasks & completed tasks
+    total_tasks = len(tasks)
+    completed_tasks = [k for k, v in tasks.items() if v]
+    
     print("Employee {} is done with tasks({}/{}):".format(
-                                                          user['name'],
-                                                          len(done),
-                                                          len(todos)))
-    for i in done:
-        print("\t {}".format(i["title"]))
+        employee_name, len(completed_tasks), total_tasks))
+    
+    for task, completed in tasks.items():
+        if completed:
+            print("\t {}".format(task))
